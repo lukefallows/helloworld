f9355f00513dc4308be3d2cd32deb43b7c676c48
Luke Fallows <luke.d.fallows@icloud.com>
2019-07-05 15:16:54 -0700

    read name from query perameter

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 /*server.js*/
 const http = require('http');
-const hostname = '127.0.0.1';
+const url = require('url');
+const hostname = 'localhost';
 const port = 3000;
 const server = http.createServer(function(req, res) {
+  const parsedURL = url.parse(req.url, true);
+  const name = parsedURL.query.name || 'Stranger';
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  res.setHeader('Content-Type', 'text/html');
+  res.end(`<h1>Hello ${name}</h1>`);
 });
 server.listen(port, hostname, function() {
   console.log('Server running at http://'+ hostname + ':' + port + '/');
